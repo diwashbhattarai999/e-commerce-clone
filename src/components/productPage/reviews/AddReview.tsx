bf524ef1df83dbfefdb69c7b16ee470967844424
Diwash <diwashb999@gmail.com>
2023-10-10 09:16:35 +0545

    AddReview fit Added

--- src/components/productPage/reviews/AddReview.tsx
+++ src/components/productPage/reviews/AddReview.tsx
@@ -1,46 +1,49 @@
 "use client";
 
 import { CustomProductType } from "@/app/product/[slug]/page";
-import Select from "./Select";
+import Select, { styleType } from "./Select";
 import { useState } from "react";
 
 interface AddReviewProps {
   product: CustomProductType | undefined;
 }
 
-export type styleType = {
-  color: string;
-  image: string;
-};
+let fits = ["Small", "True to size", "Large"];
 
 const AddReview: React.FC<AddReviewProps> = ({ product }) => {
   const [size, setSize] = useState<string | styleType>("");
   const [style, setStyle] = useState<string | styleType>("");
+  const [fit, setFit] = useState<string | styleType>("");
 
   return (
     <div>
-      <div className="flex gap-2">
+      <div className="flex gap-2 justify-end">
         {/* Size */}
-        <div className="flex items-center gap-2 font-semibold text-lg">
-          Size:
-          <Select
-            property={size}
-            selectText="Size"
-            data={product?.allSizes.filter((x) => x.size !== size)}
-            handleClick={setSize}
-          />
-        </div>
+        <Select
+          property={size}
+          selectText="Size"
+          selectTitle="Sizes:"
+          data={(product?.allSizes || []).filter((x) => x.size !== size) || []}
+          handleClick={(newItem) => setSize(newItem)}
+        />
 
         {/* Style */}
-        <div className="flex items-center gap-2 font-semibold text-lg">
-          Styles:
-          <Select
-            property={style}
-            selectText="Style"
-            data={product?.colors.filter((x) => x !== style)}
-            handleClick={setStyle}
-          />
-        </div>
+        <Select
+          property={style}
+          selectText="Style"
+          selectTitle="Styles:"
+          data={(product?.colors || []).filter((x) => x !== style) || []}
+          handleClick={(newItem) => setStyle(newItem)}
+        />
+
+        {/* Fit */}
+        <Select
+          property={fit}
+          selectText="Fit"
+          selectTitle="How does it fit:"
+          data={fits.filter((x) => x !== fit)}
+          handleClick={(newItem) => setFit(newItem)}
+        />
       </div>
     </div>
   );
